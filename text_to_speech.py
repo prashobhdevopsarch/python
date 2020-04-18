e6c7b1b0f69e5bea9ad241d655b2e5a830139472
prashobhdevopsarch <prashobhchandra@gmail.com>
2020-04-18 21:22:42 +0530

    speak

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,8 +1,9 @@
 from gtts import gTTS
 import os
 language = 'en'
+val = input("Enter your value: ") 
 def speak(mytext):
     output = gTTS(text=mytext, lang=language, slow=False)
     output.save("outp.mp3")
     os.system("start outp.mp3")
-speak("hi Guru")
+speak(val)
